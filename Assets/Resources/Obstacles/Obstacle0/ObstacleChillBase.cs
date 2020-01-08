e5ce0ce6851c6aec483edffa075f9f1eccb4f5fb
dogananil <33007291+dogananil@users.noreply.github.com>
2020-01-08 11:51:04 +0300

    Obstacle0 chill base is added with own script

---
+++ Assets/Resources/Obstacles/Obstacle0/ObstacleChillBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class ObstacleChillBase : Obstacle
+{
+   
+   
+
+    private void Awake()
+    {
+        this.obstacleType = TYPE.TYPE0;
+       
+    }
+
+  
+
+    public override void Smash()
+    {
+       // StartCoroutine(PlayAnimation());
+        // Destroy(gonnaDie);
+    }
+
+    public override IEnumerator PlayAnimation()
+    {
+        float timeStep = 0f;
+        while (timeStep < 1f)
+        {
+           
+            timeStep += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        TabController.INSTANCE.run = true;
+    }
+
+}
