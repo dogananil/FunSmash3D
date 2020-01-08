ad343ab3b16b50439cd99d2f0eb4c0336bbbb34a
dogananil <33007291+dogananil@users.noreply.github.com>
2020-01-08 10:16:17 +0300

    new obstacle is added

--- Assets/Resources/Obstacles/Obstacle2/Hammer/ObstacleHammer.cs
+++ Assets/Resources/Obstacles/Obstacle2/Hammer/ObstacleHammer.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class ObstacleHammer : MonoBehaviour
+public class ObstacleHammer : Obstacle
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject hammer;
+    [SerializeField] private AnimationCurve speedCurve2;
+    [System.NonSerialized] private Rigidbody body;
+    [System.NonSerialized] private Quaternion start;
+    [System.NonSerialized] private Quaternion end = new Quaternion(-90,0,0,0);
+
+    private void Awake()
     {
+        this.type = TYPE.TYPE1;
+       
+    }
+
+    private void Start()
+    {
+        start =hammer.transform.localRotation;
+    }
+
+    public override void Smash()
+    {
+        StartCoroutine(PlayAnimation());
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override IEnumerator PlayAnimation()
     {
-        
+        float timeStep = 0f;
+        while (timeStep < 1f)
+        {
+            // body.MovePosition(start + body.transform.forward * speedCurve2.Evaluate(timeStep) * 10.0f);
+            //hammer.transform.rotation = Quaternion.Slerp(start, end , speedCurve2.Evaluate(timeStep));
+            hammer.transform.rotation = Quaternion.RotateTowards(start, end , speedCurve2.Evaluate(timeStep));
+            timeStep += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        TabController.INSTANCE.run = true;
     }
+
 }
