5dfedb48b34e99dba6bbd8b67269d2afa757bbaf
dogananil <33007291+dogananil@users.noreply.github.com>
2020-01-21 00:00:40 +0300

    comment lines on scrollbar.cs

--- Assets/ScrollBar.cs
+++ Assets/ScrollBar.cs
@@ -28,31 +28,31 @@ public class ScrollBar : MonoBehaviour
     public void LoadProgessBar()
     {
         slider.value = (float)((float)LevelManager.instance.levelProperties.crowdSize-LevelManager.instance.currentCrowd.transform.childCount) /(float)LevelManager.instance.levelProperties.crowdSize;
-        deathCount.text = ((float)LevelManager.instance.levelProperties.crowdSize - LevelManager.instance.currentCrowd.transform.childCount) + "/" + (float)LevelManager.instance.levelProperties.crowdSize;
-        if(percantageLevelFinish.value<=slider.value && !confetiBool)
-        {
-            skullIcon.color = Color.green;
-            ParticlePool.instance.PlaySystem(LevelManager.instance.currentEnemy.transform.position, ParticlePool.SYSTEM.CONFETTI_SYSTEM, Color.black);
-            //ParticlePool.instance.PlaySystem(LevelManager.instance.enemyPieces[TabController.INSTANCE.tabCount-2].transform.position, ParticlePool.SYSTEM.CONFETTI_TRAIL_SYSTEM, Color.black);
-            confetiBool = true;
-            LevelManager.instance.canNextLevel = true;
-            needToKill.color = Color.green;
-        }
+       // deathCount.text = ((float)LevelManager.instance.levelProperties.crowdSize - LevelManager.instance.currentCrowd.transform.childCount) + "/" + (float)LevelManager.instance.levelProperties.crowdSize;
+        //if(percantageLevelFinish.value<=slider.value && !confetiBool)
+        //{
+        //    //skullIcon.color = Color.green;
+        //    //ParticlePool.instance.PlaySystem(LevelManager.instance.currentEnemy.transform.position, ParticlePool.SYSTEM.CONFETTI_SYSTEM, Color.black);
+        //    ////ParticlePool.instance.PlaySystem(LevelManager.instance.enemyPieces[TabController.INSTANCE.tabCount-2].transform.position, ParticlePool.SYSTEM.CONFETTI_TRAIL_SYSTEM, Color.black);
+        //    //confetiBool = true;
+        //   // LevelManager.instance.canNextLevel = true;
+        //    //needToKill.color = Color.green;
+        //}
     }
     public void StartProgressBar()
     {
         deathCount.text = ((float)LevelManager.instance.levelProperties.crowdSize - LevelManager.instance.currentCrowd.transform.childCount) + "/" + LevelManager.instance.levelProperties.crowdSize;
         levelText.text = (LevelManager.instance.level+1).ToString();
-        percantageLevelFinish.value = LevelManager.instance.levelProperties.percantageLevelFinish;
-        needToKill.text = "x" + (int)(LevelManager.instance.levelProperties.crowdSize * LevelManager.instance.levelProperties.percantageLevelFinish);
+       // percantageLevelFinish.value = LevelManager.instance.levelProperties.percantageLevelFinish;
+        //needToKill.text = "x" + (int)(LevelManager.instance.levelProperties.crowdSize * LevelManager.instance.levelProperties.percantageLevelFinish);
         
     }
     public void ResetProgressBar()
     {
         slider.value = 0;
-        confetiBool = false;
-        needToKill.color = Color.white;
-        skullIcon.color = Color.white;
+        //confetiBool = false;
+        //needToKill.color = Color.white;
+        //skullIcon.color = Color.white;
 
     }
 }
